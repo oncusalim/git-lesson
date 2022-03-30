8c44f38a66f76b63ef30cfc38beee5dfcbfab9ee
oncusalim <salimoncu.fb@gmail.com>
2022-03-30 16:33:28 +0300

    work 1 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,7 @@
 public class HelloWorld {
+
+// doing some work here (you can type anything you want)
+
 }
 
 
@@ -12,10 +15,33 @@ public class HelloWorld {
   4. Now instruct git to keep track of this project folder by enabling version control to create new local repository
         1. You can do this by going to top menu 'VCS' -> 'enable Version Control' and hit 'OK'
         2. 'VCS' option will turn into 'Git' from this point on.
-        3. You will see success message on bottom left corner saying created git repository in xxxx location.
-        4. If you see version not supported on bottom left corner
+        3.  From the 'Git' option click 'commit'
+        4. It will open up tab on the left
+            1. Select all the files you want to keep track of
+            2.Enter commit message into the box right on top of commit button
+            3. And hit commit
+        5. if it's the first time, it will ask you to enter 'name and email'
+             1. enter the name you desired
+             2. enter the same email you used for Github.com account
+        4. You will see success message on bottom left corner saying created git repository in xxxx location.
+        5. If you see version not supported on bottom left corner
            1. click on configure link and click on next screen.
-   5.
+
+   5.Now do some more work (add some text anywhere)
+        1. 'git' will see what has changed from last commit till now
+        2. IntelliJ will also indicate with green bar on left for indication
+        3. Now when we are ready to save this change into git repository, we can make our second commit same way we did previously
+
+
+
+
+     Task 2: Set your username and email
+
+     Set your username:
+     git config --global  user.name "FIRST_NAME_LAST_NAME"
+
+     Set your email address:
+     git config --global  user.email "MY_NAME@evxample.com"
 
 
  */
