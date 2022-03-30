d8d06b05dcc559336224d0579ff8fe4a0feede1a
oncusalim <salimoncu.fb@gmail.com>
2022-03-30 15:32:36 +0300

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+}
+
+
+/*
+
+  #Task1:
+
+  1. Open your Intellij and create new Java Project
+  2. Name your 'git-lessons'
+  3. Create a class called 'HelloWorld' under source
+  4. Now instruct git to keep track of this project folder by enabling version control to create new local repository
+        1. You can do this by going to top menu 'VCS' -> 'enable Version Control' and hit 'OK'
+        2. 'VCS' option will turn into 'Git' from this point on.
+        3. You will see success message on bottom left corner saying created git repository in xxxx location.
+        4. If you see version not supported on bottom left corner
+           1. click on configure link and click on next screen.
+   5.
+
+
+ */
