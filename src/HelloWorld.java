037d709299347d4827e74dc136166c3cd7127c18
oncusalim <salimoncu.fb@gmail.com>
2022-03-31 16:27:09 +0300

    New file is created

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,7 +8,6 @@ public class HelloWorld {
     //Doing awesome work 3 !
 }
 
-
 /*
 
   #Task1:
@@ -35,6 +34,51 @@ public class HelloWorld {
         1. 'git' will see what has changed from last commit till now
         2. IntelliJ will also indicate with green bar on left for indication
         3. Now when we are ready to save this change into git repository, we can make our second commit same way we did previously
+        4. Go to top menu 'Git' -> 'Commit'
+           1. From the left tab select the file change you wanna commit
+           2. Provide commit messages
+           3. Hit commit
+
+    #  Sharing Local Git Repository to GitHub
+
+        1. Go to 'preference' on 'Mac' and 'File'->'Settings' on Windows
+        2. Search for 'GitHub' from the preference searchbox
+        3. Click on 'GitHub from the left tab search result
+        4. Click on 'Add Account' -> 'Login via GitHub'
+            1. It will open up browser window
+            2. Authorize Jetbrain
+            3. Enter your credential if it's asked
+            4. Authorize 'Cybertek-B25' organization
+            5. Enter Okta credentials if asked
+            6. Now you will see success message saying its connected
+            7. Now you can directly interact with GitHub from you IntelliJ
+        5. Back to intelliJ and click 'OK' to save your progress
+        6. Make sure you committed all you changes
+        7. From top menu 'Git' -> GitHub' -> 'Share Project on GitHub'
+            1. Optionally provide description
+            2. Hit 'Share' button
+            3. After a while it will give you success message with direct link
+         8. Here is what exactly happened from previous process once you click on share button
+            1.It created new Remote Repository Under your GitHub Account
+            2. It created connection between your local repository on your computer and remote repository on GitHub.com
+            3. It pushed all the local commits to remote repository
+            4. All these steps are happening in one shot behind the scenes
+            5. So we just need to do it once 'Share Project on GitHub'
+         9. What if I have more work and I have more commits
+            1. Make your changes and commit your changes
+            2. DO NOT USE SHARE PROJECT ON GITHUB because it already established the connection between local remote.
+            3. All you need to do is from the top menu
+               1. 'Git' ->'Push'
+               2. It will push your local commits to remote repository.
+               3. 'Push' is a git term for pushing local commits to remote repository.
+               4. Now your local repo is in sync with remote repo
+          10. Create one more class called 'Practice1'
+          11. Observe the pop-up saying :
+               - 'Do you want to add following file to git'
+               - Click on 'Do not Ask' checkbox and click 'Add'
+               - This will ensure git will always keep track of new files added
+               - So you do not have to manually add
+           12.
 
 
 
